fff57f3115880538e0b91c1eb7671a04b01a2e41
Паплёвка Владислав Сергеевич <VPaplevka@alfabank.ru>
2025-10-07 17:01:06 +0300

    feat(init): add initial files

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+const app = express();
+
+const port = '3000';
+
+app.get('/', (req, res) => {
+  console.log(req.ip);
+  res.send('<b>Hello World!</b>');
+  console.log('Response sent');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
